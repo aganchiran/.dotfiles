7f52c3be0f2dd3c528a6a9bc81d31ad3158e2bfa
aganchiran <aganchiran@proton.me>
2022-10-21 16:22:28 +0200

    AWESOME: Add flameshot shortcut

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -163,6 +163,8 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
+    awful.key({                   }, "Print", function () awful.spawn("flameshot gui") end, {description = "show help", group="awesome"}),
+
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,     {description = "show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,          {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext,          {description = "view next", group = "tag"}),
